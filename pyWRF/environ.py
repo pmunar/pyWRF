0d1a525c57f693f28b4ff219eb72ffcfd907a976
pmunar <pere.munaradrover@gmail.com>
2019-04-09 17:01:47 +0200

    some important changes

---
+++ pyWRF/environ.py
@@ -0,0 +1,17 @@
+import os
+
+
+WRF_DIR = os.environ.get('WRF_DIR', '')
+WRF_DATA_DIR = os.environ.get('WRF_DATA_DIR', '')
+WRF_WPS_DIR = os.environ.get('WRF_WPS_DIR', '')
+WRF_WRFV3_DIR = os.environ.get('WRF_WRFV3_DIR', '')
+WRF_ARWpost_DIR = os.environ.get('WRF_ARWpost_DIR', '')
+
+DIRS = {
+    'WRF_DIR' : WRF_DIR,
+    'WRF_DATA': WRF_DATA_DIR,
+    'WRF_WPS' : WRF_WPS_DIR,
+    'WRF_WRFV3' : WRF_WRFV3_DIR,
+    'WRF_ARWpost' : WRF_ARWpost_DIR
+}
+

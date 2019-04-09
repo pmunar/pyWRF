bc76a686ae53c2b6c9711d0ed9c86573d4d379c0
pmunar <pere.munaradrover@gmail.com>
2019-04-09 17:03:50 +0200

    some important changes

---
+++ pyWRF/read_and_unzip.py
@@ -0,0 +1,17 @@
+import glob
+import os
+
+
+def gunzip_and_rename_files(path):
+    try:
+        print('Decompressing all data files in this folder')
+        print('Changing all file names. The original name is kept cut after grib2')
+        for i, filename in enumerate(glob.glob(path + '*.gz')):
+            os.system('gunzip '+filename)
+            filename = filename.split('.gz')[0]
+            new_filename = filename.split('grib2')[0]+'grib2'
+            os.rename(filename, new_filename)
+    except:
+        print('There are no files to decompress or rename. Exiting')
+
+

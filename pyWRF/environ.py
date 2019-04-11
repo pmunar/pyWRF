62cd67af0a36af5a31627a8b83fc54aa94a854e1
pmunar <pere.munaradrover@gmail.com>
2019-04-11 12:12:01 +0200

    added DIRS, since it desappeared

--- pyWRF/environ.py
+++ pyWRF/environ.py
@@ -5,3 +5,11 @@ WRF_DATA = os.environ.get('WRF_DATA', '')
 WRF_WPS = os.environ.get('WRF_WPS', '')
 WRF_WRFV3 = os.environ.get('WRF_WRFV3', '')
 WRF_ARWpost = os.environ.get('WRF_ARWpost', '')
+
+DIRS = {
+    'WRF_DIR' : WRF_DIR,
+    'WRF_DATA': WRF_DATA_DIR,
+    'WRF_WPS' : WRF_WPS_DIR,
+    'WRF_WRFV3' : WRF_WRFV3_DIR,
+    'WRF_ARWpost' : WRF_ARWpost_DIR
+}

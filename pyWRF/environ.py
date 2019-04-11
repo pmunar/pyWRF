0f1194b2b0eef1d7114285d0b952aa92eae85716
pmunar <pere.munaradrover@gmail.com>
2019-04-11 15:18:23 +0200

    corrected typos in code

--- pyWRF/environ.py
+++ pyWRF/environ.py
@@ -1,10 +1,10 @@
 import os
 
 WRF_DIR = os.environ.get('WRF_DIR', '')
-WRF_DATA = os.environ.get('WRF_DATA', '')
-WRF_WPS = os.environ.get('WRF_WPS', '')
-WRF_WRFV3 = os.environ.get('WRF_WRFV3', '')
-WRF_ARWpost = os.environ.get('WRF_ARWpost', '')
+WRF_DATA_DIR = os.environ.get('WRF_DATA', '')
+WRF_WPS_DIR = os.environ.get('WRF_WPS', '')
+WRF_WRFV3_DIR = os.environ.get('WRF_WRFV3', '')
+WRF_ARWpost_DIR = os.environ.get('WRF_ARWpost', '')
 
 DIRS = {
     'WRF_DIR' : WRF_DIR,

bbd1e57a1746a9131aa20f46accbab56f8dbc7de
pmunar <pere.munaradrover@gmail.com>
2019-04-11 17:13:07 +0200

    corrected the function

--- pyWRF/read_config.py
+++ pyWRF/read_config.py
@@ -7,9 +7,9 @@ def get_config_parameters(config_file):
     end_date = Config.get('Analysis', 'end_date')[1:-1]
     group_of_days = Config.get('Analysis', 'group_of_days')
     data_path = Config.get('Data', 'data_path')
-    out_wps = Config.get('Data', 'wps_out')[1:-1]
-    out_wrf = Config.get('Data', 'wrf_out')[1:-1]
-    out_grads = Config.get('Data', 'grads_out')[1:-1]
+    out_wps = Config.get('Data', 'wps_out')
+    out_wrf = Config.get('Data', 'wrf_out')
+    out_grads = Config.get('Data', 'grads_out')
 
     return data_path, out_wps, out_wrf, out_grads, start_date, end_date, int(group_of_days)
 

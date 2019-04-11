af6b02e8b1678f463656d9a64abf77331ef07879
pmunar <pere.munaradrover@gmail.com>
2019-04-11 12:25:36 +0200

    corrected some functions

--- bin/wrf_analysis.py
+++ bin/wrf_analysis.py
@@ -35,6 +35,9 @@ def __main__():
         if n == range(number_of_groups[0] + 1)[-1]:
             stop_time = get_stop_date_for_processing_last_group(end_date_datetime)
         analysis = RunAnalysis(start_time, stop_time, )
+        analysis.run_wps()
+        analysis.run_WRF()
+        analysis.clean_directories()
 
         print('Analyzing times between +'start_time, stop_time, data_path, wps_out)
         start_time = stop_time + datetime.timedelta(hours=6)

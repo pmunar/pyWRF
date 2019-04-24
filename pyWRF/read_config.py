65c716f25cc9e07592ade0fea0a06f3d12401d2a
pmunar <pere.munaradrover@gmail.com>
2019-04-24 11:20:36 +0200

    changed some output directories paths

--- pyWRF/read_config.py
+++ pyWRF/read_config.py
@@ -7,11 +7,9 @@ def get_config_parameters(config_file):
     end_date = Config.get('Analysis', 'end_date')[1:-1]
     group_of_days = Config.get('Analysis', 'group_of_days')
     data_path = Config.get('Data', 'data_path')
-    out_wps = Config.get('Data', 'wps_out')
-    out_wrf = Config.get('Data', 'wrf_out')
-    out_grads = Config.get('Data', 'grads_out')
+    output_path = Config.get('Data', 'output_path')
 
-    return data_path, out_wps, out_wrf, out_grads, start_date, end_date, int(group_of_days)
+    return data_path, output_path, start_date, end_date, int(group_of_days)
 
 # start_date_datetime = conf_date_to_datetime(start_date)
 # end_date_datetime = conf_date_to_datetime(end_date)

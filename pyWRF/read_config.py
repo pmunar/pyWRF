6ab2bdc63ce9a62d7717616c18467e3203e875a5
pmunar <pere.munaradrover@gmail.com>
2019-04-24 11:22:39 +0200

    removed commented lines

--- pyWRF/read_config.py
+++ pyWRF/read_config.py
@@ -10,9 +10,3 @@ def get_config_parameters(config_file):
     output_path = Config.get('Data', 'output_path')
 
     return data_path, output_path, start_date, end_date, int(group_of_days)
-
-# start_date_datetime = conf_date_to_datetime(start_date)
-# end_date_datetime = conf_date_to_datetime(end_date)
-#
-# length_of_analysis = end_date_datetime - start_date_datetime
-# print('Length of the analysis: {:d} days'.format(length_of_analysis.days))

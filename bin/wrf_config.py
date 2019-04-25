ab40e2949dc0c779e877e9090613367966d25eef
Pere Munar <pere@iMac.local>
2019-04-25 10:53:17 +0200

    corrected bug

--- bin/wrf_config.py
+++ bin/wrf_config.py
@@ -7,8 +7,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('config', help='The name of the configuration file')
 args = parser.parse_args()
 
-if not os.path.exists('./'args.config):
-    os.system('cp $PYWRF_DIR/config_file/config_example.conf '+args.config)
+if not os.path.exists('./'+args.config):
+    os.system('cp $PYWRF_DIR/config_file/config_example.conf ' + args.config)
 else:
     answer = input('The configuration file exists in current path.\n Do you want to overwrite it? [y/n]')
     if answer == 'y':

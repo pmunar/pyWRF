d2d4b628757629d31b5f86b6770c5d7df2c4dad3
pmunar <pmunar@neas.uab.es>
2019-04-25 17:57:26 +0200

    removed folder

--- pywrf/environ.py
+++
@@ -1,7 +0,0 @@
-import os
-
-WRF_DIR = os.environ.get('WRF_DIR', '')
-WRF_DATA = os.environ.get('WRF_DATA', '')
-WRF_WPS = os.environ.get('WRF_WPS', '')
-WRF_WRFV3 = os.environ.get('WRF_WRFV3', '')
-WRF_ARWpost = os.environ.get('WRF_ARWpost', '')

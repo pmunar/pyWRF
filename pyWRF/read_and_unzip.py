e1679205b900dd64cce2798b912de047f680889c
pmunar <pere.munaradrover@gmail.com>
2019-04-25 11:46:44 +0200

    adapted for format of data input files

--- pyWRF/read_and_unzip.py
+++ pyWRF/read_and_unzip.py
@@ -2,7 +2,7 @@ import glob
 import os
 
 
-def gunzip_and_rename_files(path):
+def gunzip_and_rename_files(path, format):
     """
     Function that performs the decompression of the input files, if they are still in gzip format. If they are already
     decompressed, it does nothing
@@ -12,12 +12,12 @@ def gunzip_and_rename_files(path):
     try:
         print('Decompressing all data files in this folder')
         print('Changing all file names. The original name is kept cut after grib2')
-        for i, filename in enumerate(glob.glob(path + '/*.gz')):
-            os.system('gunzip '+filename)
-            filename = filename.split('.gz')[0]
-            new_filename = filename.split('grib2')[0]+'grib2'
+        for i, filename in enumerate(glob.glob(path + '/*.*')):
+            if filename.lower().endswith('.gz'):
+                os.system('gunzip '+filename)
+                new_filename = filename.split(format)[0]+ format
+            else:
+                new_filename = filename.split(format)[0] + format
             os.rename(filename, new_filename)
     except:
         print('There are no files to decompress or rename. Exiting')
-
-

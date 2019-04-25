621fc8e2f721f00ec3aed7e0c474025ab50f0d9a
Pere Munar <pere@iMac.local>
2019-04-25 10:46:32 +0200

    created script that copies the example config file into working directory

---
+++ bin/wrf_config.py
@@ -0,0 +1,16 @@
+import os
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('config', help='The name of the configuration file')
+args = parser.parse_args()
+
+if not os.path.exists('./'args.config):
+    os.system('cp $PYWRF_DIR/config_file/config_example.conf '+args.config)
+else:
+    answer = input('The configuration file exists in current path.\n Do you want to overwrite it? [y/n]')
+    if answer == 'y':
+        os.system('cp $PYWRF_DIR/config_file/config_example.conf ' + args.config)
+    elif answer == 'n' or answer != 'y':
+        sys.exit()

bf314c3d1c32ebbdff6596b3ec4a94556d9cf64d
Pere Munar <pere@iMac.local>
2019-04-25 10:54:54 +0200

    corrected asking sentence

--- bin/wrf_config.py
+++ bin/wrf_config.py
@@ -10,7 +10,7 @@ args = parser.parse_args()
 if not os.path.exists('./'+args.config):
     os.system('cp $PYWRF_DIR/config_file/config_example.conf ' + args.config)
 else:
-    answer = input('The configuration file exists in current path.\n Do you want to overwrite it? [y/n]')
+    answer = input('The configuration file exists in current path.\n Do you want to overwrite it? [y/n]: ')
     if answer == 'y':
         os.system('cp $PYWRF_DIR/config_file/config_example.conf ' + args.config)
     elif answer == 'n' or answer != 'y':

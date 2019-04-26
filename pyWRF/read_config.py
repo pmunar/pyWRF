268fa9a0500486a7dca83e42757e2138c118c5a1
pmunar <pere.munaradrover@gmail.com>
2019-04-26 16:56:00 +0200

    corrected some bugs

--- pyWRF/read_config.py
+++ pyWRF/read_config.py
@@ -8,15 +8,15 @@ def get_config_parameters(config_file):
     Config = configparser.ConfigParser()
     Config.read(config_file)
     Config.sections()
-    start_date = Config.get('Analysis', 'start_date')[1:-1]
-    end_date = Config.get('Analysis', 'end_date')[1:-1]
-    group_of_days = Config.get('Analysis', 'group_of_days')
     data_path = Config.get('Data', 'data_path')
     output_path = Config.get('Data', 'output_path')
     data_format = Config.get('Data', 'data_format')
+    hours_step = Config.get('Data', 'hours_step')
+    input_data_server = Config.get('Data', 'input_data_server')
+    start_date = Config.get('Analysis', 'start_date')[1:-1]
+    end_date = Config.get('Analysis', 'end_date')[1:-1]
+    group_of_days = Config.get('Analysis', 'group_of_days')
     num_domains = Config.get('Analysis', 'num_domains')
-    hours_step = Config.get('Analysis', 'hours_step')
-    input_data_server = Config.get('Analysis', 'input_data_server')
 
     return data_path, output_path, data_format, start_date, end_date, int(num_domains), int(group_of_days), \
            int(hours_step), input_data_server
